85e90700075c01e23617462b38e596fa324d9575
Ajmain Muhtady Joarder <fahimajmain@gmail.com>
2024-04-07 03:39:00 +0600

    Ex1

---
+++ Exercise/Ex1.py
@@ -0,0 +1,24 @@
+'''
+Code Basics
+'''
+'''Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.'''
+#Break is a keyword.
+
+'''Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables'''
+
+'''birthYear = 2000
+CurrentYear = 2024
+myAge = CurrentYear-birthYear
+print("My age is",myAge)
+'''
+
+
+'''Store your first, middle and last name in three different variables and then print your full name using these variables'''
+
+first = "Ajmain "
+middle = "Muhtady "
+last = "Joarder"
+print(first+middle+last)
+
+
+'''Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue'''
